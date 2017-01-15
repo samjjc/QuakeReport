1f91934874ab9879afdda8d36098d446df7168fc
samjjc <sam111jc@gmail.com>
2017-01-15 15:02:11 -0500

    ui polish, added color circles

--- app/src/main/java/com/example/android/quakereport/QuakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/QuakeAdapter.java
@@ -1,7 +1,8 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +26,6 @@ public class QuakeAdapter extends ArrayAdapter<Quake> {
         super(context, 0, quakeAdapter);
     }
 
-    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItemView = convertView;
@@ -36,13 +36,24 @@ public class QuakeAdapter extends ArrayAdapter<Quake> {
 
         Quake currentQuake = getItem(position);
 
+        //magnitude formatting
+
+
+
         DecimalFormat formatter = new DecimalFormat("0.0");
 
 
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_text_view);
+
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentQuake.getmMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
+
         magnitudeTextView.setText(formatter.format(currentQuake.getmMagnitude()));
 
-        
+
+
+        // Location formatting
 
         String originalLocation = currentQuake.getmPlace();
         String place;
@@ -88,4 +99,45 @@ public class QuakeAdapter extends ArrayAdapter<Quake> {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(dateObject);
     }
+
+    private int getMagnitudeColor(double magnitude){
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
 }
+
